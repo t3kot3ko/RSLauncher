c591dd7fc7ca128793101725a66b2eb48a0db625
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2012-11-27 15:55:32 +0900

    First commit for RSLauncher

---
+++ src/rslauncher/popup/actions/ScriptLauncher.java
@@ -0,0 +1,44 @@
+package rslauncher.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class ScriptLauncher implements IObjectActionDelegate {
+
+	private Shell shell;
+	
+	/**
+	 * Constructor for Action1.
+	 */
+	public ScriptLauncher() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		MessageDialog.openInformation(
+			shell,
+			"RSLauncher",
+			"New Action was executed.");
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
